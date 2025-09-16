3ed872352198aa8d7423858736d250c3cecb87c4
MariaTere24 <mariateresaalarcongarcia@gmail.com>
2025-09-16 15:39:51 -0500

    feat: agregar endpoint para listar usuarios en API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,16 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        // Configurar CORS globalmente
+        $middleware->web(append: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
+        
+        $middleware->api(append: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
